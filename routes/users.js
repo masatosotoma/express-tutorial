953e0cfde1454cb75deb539b2c37497efdc4bf29
MasatoSotoma <masatosotoma@gmail.com>
2025-02-06 22:57:18 -0500

    Route requestes organized

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,17 @@ router.post("/", (req, res) => {
   res.send("Create User");
 });
 
-router.get("/:id", (req, res) => {
-  res.send(`Get User with ID ${req.params.id}`);
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get User with ID ${req.params.id}`);
+  })
+  .put("/:id", (req, res) => {
+    res.send(`Update User with ID ${req.params.id}`);
+  })
+  .delete("/:id", (req, res) => {
+    res.send(`Delete User with ID ${req.params.id}`);
+  });
+
 
 module.exports = router;
