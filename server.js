50083a6030952343de143ac46ac2740728fcda1d
MasatoSotoma <masatosotoma@gmail.com>
2025-02-23 22:57:48 -0500

    ejs html file for submit form

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", logger,(req, res) => {
-  console.log("Here");
-  res.render("index", { text: "World" });
-});
+app.set("view engine", "ejs");
 
 const userRouter = require("./routes/users");
 
