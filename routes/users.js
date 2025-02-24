6e381505749080c1942ddb4d0ff46aa5d2adb49d
MasatoSotoma <masatosotoma@gmail.com>
2025-02-23 22:58:44 -0500

    Updated middleware + server

--- routes/users.js
+++ routes/users.js
@@ -4,38 +4,44 @@ const router = express.Router();
 router.use(logger);
 
 router.get("/", (req, res) => {
+  console.log(req.query.name);
   res.send("User List");
 });
 
 router.get("/new", (req, res) => {
-  res.send("User New Form");
+  res.render("users/new");
 });
 
 router.post("/", (req, res) => {
-  res.send("Create User");
+  const isValid = false;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
 });
 
 router
   .route("/:id")
   .get((req, res) => {
     console.log(req.user);
-    res.send(`Get User with ID ${req.params.id}`);
+    res.send(`Get User With ID ${req.params.id}`);
   })
   .put((req, res) => {
-    res.send(`Update User with ID ${req.params.id}`);
+    res.send(`Update User With ID ${req.params.id}`);
   })
   .delete((req, res) => {
-    res.send(`Delete User with ID ${req.params.id}`);
+    res.send(`Delete User With ID ${req.params.id}`);
   });
 
 const users = [{ name: "Kyle" }, { name: "Sally" }];
-
 router.param("id", (req, res, next, id) => {
-  req.user - users[id];
+  req.user = users[id];
   next();
 });
 
-//creating middleware
 function logger(req, res, next) {
   console.log(req.originalUrl);
   next();
